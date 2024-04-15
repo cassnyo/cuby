4dc97d057829057992b6ad82f121e51178f9301f
Adrián Belmonte <cassnyo@gmail.com>
2024-04-15 19:31:02 +0200

    Add flag to keep screen awake

--- app/src/main/java/com/cassnyo/cuby/MainActivity.kt
+++ app/src/main/java/com/cassnyo/cuby/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.cassnyo.cuby
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -20,6 +21,8 @@ import org.worldcubeassociation.tnoodle.puzzle.ThreeByThreeCubePuzzle
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
         setContent {
             CubyTheme {
                 // A surface container using the 'background' color from the theme
